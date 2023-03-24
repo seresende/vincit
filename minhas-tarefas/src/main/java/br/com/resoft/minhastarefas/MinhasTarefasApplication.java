6b06f0f35556c255fadf6dcfe258e701b0ac1d89
Sérgio Luiz Resende <sergio.resende@compasso.com.br>
2023-03-24 15:56:04 -0300

    Inclusao do Projeto

---
+++ minhas-tarefas/src/main/java/br/com/resoft/minhastarefas/MinhasTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.resoft.minhastarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhasTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhasTarefasApplication.class, args);
+	}
+
+}
---
+++ minhas-tarefas/src/test/java/br/com/resoft/minhastarefas/MinhasTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.resoft.minhastarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
